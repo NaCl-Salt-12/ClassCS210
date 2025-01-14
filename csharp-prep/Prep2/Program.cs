b780cecfce2d8696e94705700550e166386f068f
NaCl-Salt-12 <nathanieleclark@gmail.com>
2025-01-14 15:58:04 -0700

    doing assignment prep 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage: ");
+        string user_input = Console.ReadLine();
+        int grade = int.Parse(user_input);
+
+       if (grade >= 90) 
+            {
+                Console.WriteLine("You got an A");
+            }
+        else if(grade >= 80)
+            {
+                Console.WriteLine("You got a B.");
+            }
+        else if(grade >= 70)
+            {
+                Console.WriteLine("You got a C.");
+            }
+         else if(grade >= 60)
+            {
+                Console.WriteLine("You got a D");
+            }
+        else 
+            {
+                Console.WriteLine("You got a F");
+            }
+          
     }
 }
